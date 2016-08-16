7342aa78fb62bdc2edad12beea78eb59b52324a7
Sydni Topper <sydni.topper.18@dartmouth.edu>
2016-08-16 15:27:26 -0400

    all

--- app/server.js
+++ app/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+// at top
+import socketio from 'socket.io';
+import http from 'http';
+
+import Notes from './controllers/note-controller';
 
 // initialize
 const app = express();
+// add server and io initialization after app
+const server = http.createServer(app);
+const io = socketio(server);
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
@@ -18,9 +28,41 @@ app.get('/', (req, res) => {
   res.send('hi');
 });
 
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/notes';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
 // START THE SERVER
 // =============================================================================
 const port = process.env.PORT || 9090;
-app.listen(port);
-
+// change app.listen to server.listen
+server.listen(port);
 console.log(`listening on: ${port}`);
+
+
+io.on('connection', (socket) => {
+  // on first connection emit notes
+  Notes.getNotes().then(result => {
+    socket.emit('notes', result);
+  });
+
+  // pushes notes to everybody
+  const pushNotes = () => {
+    Notes.getNotes().then(result => {
+      // broadcasts to all sockets including ourselves
+      io.sockets.emit('notes', result);
+    });
+  };
+
+  // creates notes and
+  socket.on('createNote', (fields) => {
+    Notes.createNote(fields).then((result) => {
+      pushNotes();
+    }).catch(error => {
+      console.log(error);
+      socket.emit('error', 'create failed');
+    });
+  });
+});
